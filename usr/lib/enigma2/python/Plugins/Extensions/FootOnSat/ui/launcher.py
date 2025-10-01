cd9e8ad9c2a1f155fdcdc63adce2d6e0c96f327a
RAED <rrrr53@hotmail.com>
2025-10-01 22:18:34 +0300

    Update launcher.py

--- usr/lib/enigma2/python/Plugins/Extensions/FootOnSat/ui/launcher.py
+++ usr/lib/enigma2/python/Plugins/Extensions/FootOnSat/ui/launcher.py
@@ -21,6 +21,7 @@ import os
 import json
 import sys
 from sys import version_info
+from . import compat
 
 PY3 = version_info[0] == 3
 
@@ -33,6 +34,27 @@ config.plugins.FootOnSat.icons = ConfigSelection(default = "default_icons", choi
 	("icons_renkli", _("renkli icons")),
 	("italia2012_icons", _("italia2012 Full style color"))
 	])
+
+
+options = [
+    ("2", _("Two hours after the end of the match")), 
+    ("3", _("Three hours after the end of the match"))
+]
+
+finishedmatches_map = {
+    "2": 300,
+    "3": 200
+}
+
+if not hasattr(config.plugins, "FootOnSat"):
+    config.plugins.FootOnSat = ConfigSubsection()
+
+config.plugins.FootOnSat.finishedmatches = ConfigSelection(default="2", choices=options)
+
+def on_finishedmatches_change(config_element):
+	new_value = finishedmatches_map[config.plugins.FootOnSat.finishedmatches.value]
+
+config.plugins.FootOnSat.finishedmatches.addNotifier(on_finishedmatches_change, initial_call=False)
 
 VER = float(__version__)
 
@@ -421,8 +443,9 @@ class MenuFootOnSat(ConfigListScreen, Screen):
 	def createSetup(self):
 		self.configChanged = True
 		self.list = []
-		self.list.append(getConfigListEntry(_("Enable checking for Online Update"), config.plugins.FootOnSat.updateonline, _(" This option to Enable or Disable checking for Online Update")))
-		self.list.append(getConfigListEntry(_("Select Icons Style"), config.plugins.FootOnSat.icons, _(" This option to enable to select Icons Style")))
+		self.list.append(getConfigListEntry(_("Enable checking for Online Update"), config.plugins.FootOnSat.updateonline, _("This option to Enable or Disable checking for Online Update")))
+		self.list.append(getConfigListEntry(_("Select Icons Style"), config.plugins.FootOnSat.icons, _("This option to enable to select Icons Style")))
+		self.list.append(getConfigListEntry(_("Hide matches from the list"), config.plugins.FootOnSat.finishedmatches, _("This feature allows you to hide matches from the list after")))
 		self["config"].list = self.list
 		self["config"].l.setList(self.list)
 		self["config"].onSelectionChanged.append(self.updateHelp)
