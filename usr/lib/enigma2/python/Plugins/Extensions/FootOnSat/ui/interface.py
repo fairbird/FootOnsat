cf6b06df173328f977aa8b24a294b046385d9a5f
RAED <rrrr53@hotmail.com>
2025-11-06 02:35:01 +0300

    Update interface.py

--- usr/lib/enigma2/python/Plugins/Extensions/FootOnSat/ui/interface.py
+++ usr/lib/enigma2/python/Plugins/Extensions/FootOnSat/ui/interface.py
@@ -2382,7 +2382,7 @@ class StandingsScreen(Screen):
 				res.append(MultiContentEntryText(pos=(20, 0), size=(50, ITEM_HEIGHT), font=0,
 												 flags=RT_HALIGN_CENTER | RT_VALIGN_CENTER, text=str(club_idx)))
 			else:  # 2560
-				res.append(MultiContentEntryText(pos=(0, LOGO_Y_POS +12), size=(70, LOGO_SIZE_H), font=0,
+				res.append(MultiContentEntryText(pos=(0, LOGO_Y_POS +13), size=(70, LOGO_SIZE_H), font=0,
 												 flags=RT_HALIGN_CENTER | RT_VALIGN_CENTER, text=str(club_idx)))
 			club_idx += 1
 
