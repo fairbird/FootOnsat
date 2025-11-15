64d9118e9011337587e8330584e6af87415ffd0b
RAED <rrrr53@hotmail.com>
2025-11-15 14:20:44 +0300

    Add american football (NFL)

--- usr/lib/enigma2/python/Plugins/Extensions/FootOnSat/ui/launcher.py
+++ usr/lib/enigma2/python/Plugins/Extensions/FootOnSat/ui/launcher.py
@@ -172,7 +172,7 @@ class FootOnsatLauncher(Screen):
 			return
 		ordering = ["today", "championsleague", "europaleague", "ConferenceLeague", "premierleague", "laliga", "seriea",
 		"bundesliga", "ligue1", "saudiarabia", "worldcup", "afcchampions", "afcchampionstwo","championship", "cafchampions", "superLig",
-		"belgianpro", "eredivisie", "laliga2", "liganos", "basketball", "nba", "hockey", "formula1"]
+		"belgianpro", "eredivisie", "laliga2", "liganos", "basketball", "nba", "hockey", "nfl", "formula1"]
 		# Keep only items in ordering, then sort according to ordering
 		# filtered_compet = [c for c in ordering if c in compet]
 		# self.menuList = self.custom_sort(ordering, filtered_compet)
